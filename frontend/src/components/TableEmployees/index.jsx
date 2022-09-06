a7e4c2953c056c1e0cda57a84fcabd59098544fb
jsr029 <jsr059@gmail.com>
2022-09-06 09:04:07 +0200

    Page 2

---
+++ frontend/src/components/TableEmployees/index.jsx
@@ -0,0 +1,70 @@
+import React, { useMemo, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import Table from '../Table';
+
+function TableEmployees() {
+    /* 
+      - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+      - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+    */
+    const employeesStore = useSelector((state) => state.employeesReducer).data
+    console.log(employeesStore)
+
+    const columns = useMemo(
+        () => [
+            {
+                // first group - TV Show
+                Header: "Current Employees",
+                // First group columns
+                columns: [
+                    {
+                        Header: "Firstname",
+                        accessor: "firstname"
+                    },
+                    {
+                        Header: "Lastname",
+                        accessor: "lastname"
+                    },
+                    {
+                        Header: "Start Date",
+                        accessor: "startdate"
+                    },
+                    {
+                        Header: "Department",
+                        accessor: "Departement.value",
+                    },
+                    {
+                        Header: "Date of Birth",
+                        accessor: "dateofbirth"
+                    },
+                    {
+                        Header: "Street",
+                        accessor: "street"
+                    },
+                    {
+                        Header: "City",
+                        accessor: "city"
+                    },
+                    {
+                        Header: "State",
+                        accessor: "State.value",
+                   },
+                    {
+                        Header: "Zip Code",
+                        accessor: "zipcode"
+                    }
+                ]
+            }
+        ],
+        []
+    );
+
+
+    return (
+        <div className="App">
+            <Table columns={columns} data={employeesStore} />
+        </div>
+    );
+}
+
+export default TableEmployees
--- frontend/src/pages/ViewCurrentEmployees.jsx
+++ frontend/src/pages/ViewCurrentEmployees.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import EmployeesList from '../components/EmployeesList'
+import TableEmployees from '../components/TableEmployees'
 
 function ViewCurrentEmployees() {
   return (
     <>  
       <div className='employees'>
-        <EmployeesList />
+        <h1>Current Employees</h1>
+        <TableEmployees />
       </div>            
       <Link to="/">Home</Link>
     </>
